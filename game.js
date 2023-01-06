bcfd031fbc2161e6ec05b0f5b2925e61624c7d85
Craig Ostrin <40326682+craigostrin@users.noreply.github.com>
2023-01-06 14:47:34 -0800

    Add getComputerChoice()

--- game.js
+++ game.js
@@ -8,8 +8,6 @@
 const DEFAULT_PROMPT = "It's Rock/Paper/Scissors, baby!\nYou know the rules. Choose your weapon."
 const OOPS_PROMPT = "Hmm, the hand you reach out is empty.\nLuckily, I am merciful. Now pick a proper weapon."
 
-console.log(askPlayerSelection());
-
 
 function askPlayerSelection() {
     let input = prompt(DEFAULT_PROMPT);
@@ -21,4 +19,19 @@ function askPlayerSelection() {
     }
     
     return input;
+}
+
+function getComputerChoice() {
+    let choice = '';
+    let rng = Math.floor( ( Math.random() * 3 ) ); // returns 0, 1, or 2
+
+    if (rng < 1) {
+        choice = 'rock';
+    } else if (rng < 2) {
+        choice = 'paper';
+    } else {
+        choice = 'scissors';
+    }
+
+    return choice;
 }
