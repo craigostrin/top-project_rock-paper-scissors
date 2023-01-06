b692dfcf2083d400ca03de1a8e0cf0e73c711afd
Craig Ostrin <40326682+craigostrin@users.noreply.github.com>
2023-01-06 15:08:47 -0800

    Add capitalizeString() and edit input checks Input now gets converted to capitalized string up front (eg, 'Rock' instead of 'rock'), since that's how all the results will be displayed.

--- game.js
+++ game.js
@@ -1,19 +1,46 @@
 // go go gadget pseudocode
 
-// prompt for playerSelection (case insensitive!)
-// getComputerChoice() will randomly return a string 'Rock' 'Paper' or 'Scissors 
-// playRound(playerSelection, computerSelection) returns a string showing the result
+// DONE prompt for playerSelection (case insensitive!)
+// DONE getComputerChoice() will randomly return a string 'Rock' 'Paper' or 'Scissors 
+// playRound(playerSelection, computerSelection) returns a string showing result like so: 'You Lose! Paper beats Rock'
 // game() loops playRound() 5 times, then declares the winner
 
 const DEFAULT_PROMPT = "It's Rock/Paper/Scissors, baby!\nYou know the rules. Choose your weapon."
 const OOPS_PROMPT = "Hmm, the hand you reach out is empty.\nLuckily, I am merciful. Now pick a proper weapon."
 
 
+function playRound(playerSelection, cpuSelection) {
+    let result = '';
+
+
+
+
+}
+
+// work in progress
+function determineWinner(pick1, pick2) {
+    let winner = '';
+
+    if (pick1 == pick2) {
+        winner = 'Tie';
+    }
+    
+    if (pick1 == 'Rock') {
+        if (pick2 == 'Paper') {
+            winner = pick1;
+        } else if (pick2 == 'Scissors') {
+            winner = pick1;
+        }
+    }
+
+    return winner;
+}
+
 function askPlayerSelection() {
     let input = prompt(DEFAULT_PROMPT);
-    input = input.toLowerCase().trim();
+    input = capitalizeString( input.trim() );
 
-    if (input != 'rock' && input != 'paper' && input != 'scissors') {
+    if (input != 'Rock' && input != 'Paper' && input != 'Scissors') {
         alert(OOPS_PROMPT);
         return askPlayerSelection();
     }
@@ -26,12 +53,18 @@ function getComputerChoice() {
     let rng = Math.floor( ( Math.random() * 3 ) ); // returns 0, 1, or 2
 
     if (rng < 1) {
-        choice = 'rock';
+        choice = 'Rock';
     } else if (rng < 2) {
-        choice = 'paper';
+        choice = 'Paper';
     } else {
-        choice = 'scissors';
+        choice = 'Scissors';
     }
 
     return choice;
+}
+
+function capitalizeString(text) {
+    let first_letter = text[0].toUpperCase();
+    let the_rest = text.slice(-1).toLowerCase();
+    return first_letter + the_rest;
 }
